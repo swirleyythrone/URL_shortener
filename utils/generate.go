02d96699475df062c188c64857cb48c1f6702b5b
swirleythrone <aryannawathey@gmail.com>
2025-07-01 23:24:47 +0530

    Utils

---
+++ utils/generate.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"math/rand"
+	time "time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func GenerateShortID() string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, 6)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
